46a832914c68bf26cab7f7bf273ea5a270f894ba
Sirinthipjw <phuchokdee.31@gmail.com>
2024-07-14 14:31:19 +0700

    create pro workshop

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,12 +1,66 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule} from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+  service_url = 'https://66933899c6be000fa07a1844.mockapi.io/todo/v1/topic'
+  topicId:any
+  list:any =[]
+  todo = new FormControl('')
+
+  constructor(
+      private route:ActivatedRoute,
+      private http:HttpClient
+    ){
+      this.route.paramMap.subscribe(params =>{
+        console.log(params.get('topicId'))
+        this.topicId = params.get('topicId')
+        this.loadlist()
+      })
+    }
+
+  loadlist(){
+    console.log(`${this.service_url}/${this.topicId}/todos`)
+    this.http.get(`${this.service_url}/${this.topicId}/todos`).subscribe({
+      next:(result) =>{
+        console.log(result)
+        this.list = result
+      }
+    })
+  }
+
+  onAddTodo(){
+    let body = {
+       "todo": this.todo.value
+    }
+    this.http.post(`${this.service_url}/${this.topicId}/todos/`,body).subscribe({
+      next: (result) =>{
+        console.log(result)
+        this.loadlist()
+      }
+    })
+  }
+
+  onRemoveTodo(id: number){
+    console.log(id)
+    this.http.delete(`${this.service_url}/${this.topicId}/todos/${id}`).subscribe({
+      next:(result) =>{
+        console.log(result)
+        this.loadlist()
+      }
+    })
+  }
+
+
+
 
 }
+
--- src/app/topic/topic.component.ts
+++ src/app/topic/topic.component.ts
@@ -1,12 +1,64 @@
+import { routes } from './../app.routes';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-topic',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './topic.component.html',
   styleUrl: './topic.component.css'
 })
 export class TopicComponent {
+  service_url =`https://66933899c6be000fa07a1844.mockapi.io/todo/v1/topic`
+  topics:any = []
+
+  topic = new FormControl('')
+
+  onAddTopic(){
+    let body = {
+      "topic": this.topic.value
+    }
+    this.http.post(this.service_url, body).subscribe({
+      next:(result) =>{
+        console.log(result)
+        this.loadTopics()
+      }
+    })
+  }
+  onRemoveTopic(id:number){
+    console.log(id)
+    this.http.delete(`${this.service_url}/${id}`).subscribe({
+      next: (result) =>{
+        console.log(result)
+        this.loadTopics()
+      }
+    })
+  }
+  onSelectTopic(id:number){
+    console.log(id)
+    this.router.navigate(['/list',id])
+  }
+
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ){
+    this.loadTopics()
+    // http.get(this.service_url).subscribe({
+    //   next:(result) =>{
+    //     this.topics = result
+    //   }
+    // })
+  }
+  loadTopics(){
+    this.http.get(this.service_url).subscribe({
+        next:(result) =>{
+          this.topics = result
+        }
+      })
+  }
 
 }
